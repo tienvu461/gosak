b4697be5683f3943c7d7f4ecfe8c9249f36e0ac6
tienvu461 <tienvu461@gmail.com>
2022-09-25 21:29:35 +0700

    init: get command

---
+++ cmd/get.go
@@ -0,0 +1,67 @@
+/*
+Copyright © 2022 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+
+	"github.com/spf13/cobra"
+)
+
+// getCmd represents the get command
+var getCmd = &cobra.Command{
+	Use:   "get",
+	Short: "A brief description of your command",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("get called")
+        var endPoint ="https://ifconfig.me"
+
+        if len(args) >= 1 && args[0] != "" {
+            endPoint = args[0]
+        }
+
+        fmt.Println("Trying to get public IP Address")
+
+        response, err := http.Get(endPoint)
+
+        if err != nil {
+            fmt.Println(err)
+            fmt.Println("Cannot get public IP Address")
+        }
+
+        defer response.Body.Close()
+
+        if response.StatusCode == 200 {
+            body, err := io.ReadAll(response.Body)
+            if err != nil {
+                fmt.Println(err)
+            }
+            fmt.Println(body)
+        } else {
+            fmt.Println("Fail to reach" + endPoint)
+        }
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(getCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// getCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// getCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
---
+++ cmd/root.go
@@ -0,0 +1,48 @@
+/*
+Copyright © 2022 NAME HERE <EMAIL ADDRESS>
+
+*/
+package cmd
+
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+    // "github.com/spf13/viper"
+)
+
+
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "gosak",
+	Short: "a golang swiss army knife",
+	Long: `gosak is a CLI application written in Go that can be used for various purpose from such as 
+    network utilities, cli utilities, shortcut for daily use cmd, and can be customize easily`,
+	// Uncomment the following line if your bare application
+	// has an action associated with it:
+	// Run: func(cmd *cobra.Command, args []string) { },
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	// Here you will define your flags and configuration settings.
+	// Cobra supports persistent flags, which, if defined here,
+	// will be global for your application.
+
+	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.gosak.yaml)")
+
+	// Cobra also supports local flags, which will only run
+	// when this action is called directly.
+	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+
