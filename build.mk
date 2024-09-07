87ff1bb80cb2e333b7fe127b91b1208bd89eb364
tienvv@devbox.tienvv.com <tienvv@devbox.tienvv.com>
2024-09-07 14:40:36 +0000

    chore: build when tag

--- build.mk
+++ build.mk
@@ -1,4 +1,4 @@
-.PHONY: build clean
+.PHONY: build build-all clean
 
 NAME := gosak
 ENV := local
@@ -7,19 +7,35 @@ GOARCH := amd64
 HASH := $$(git rev-parse --short --verify HEAD)
 DATE := $$(date -u '+%Y%m%dT%H%M%S')
 GOVERSION = $$(go version)
+ALLOS = linux darwin
+ALLARCH = amd64 arm64
 
-build: $(NAME).$(ENV).$(GOOS).$(GOARCH)
+build: $(NAME).$(GOOS).$(GOARCH)
 
-$(NAME).$(ENV).$(GOOS).$(GOARCH):
+$(NAME).$(GOOS).$(GOARCH):
 	GOOS=$(GOOS) GOARCH=$(GOARCH) \
-	    go build -tags=$(ENV) \
+	    go build \
 	        -o $(NAME) \
 	        -ldflags "-X \"github.com/tienvu461/gosak/utils.Version=$(DATE)-$(HASH)\" \
-	-X \"github.com/tienvu461/gosak/utils.Hash=$(HASH)\" \
+	            -X \"github.com/tienvu461/gosak/utils.Hash=$(HASH)\" \
 	            -X \"github.com/tienvu461/gosak/utils.Date=$(DATE)\" \
 	            -X \"github.com/tienvu461/gosak/utils.GoVersion=$(GOVERSION)\"" \
 	        .
 	mv $(NAME) $@
 
 clean:
-	-rm -rf $(NAME).$(ENV).$(GOOS).$(GOARCH)
+	-rm -rf $(NAME).*
+
+build-all:
+	for os in $(ALLOS); do \
+	    for arch in $(ALLARCH); do \
+	        GOOS=$$os GOARCH=$$arch \
+	        go build \
+	            -o $(NAME).$$os.$$arch \
+	            -ldflags "-X \"github.com/tienvu461/gosak/utils.Version=$(DATE)-$(HASH)\" \
+	                -X \"github.com/tienvu461/gosak/utils.Hash=$(HASH)\" \
+	                -X \"github.com/tienvu461/gosak/utils.Date=$(DATE)\" \
+	                -X \"github.com/tienvu461/gosak/utils.GoVersion=$(GOVERSION)\"" \
+	                .;\
+	    done \
+	done
